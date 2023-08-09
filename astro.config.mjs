feaceb32ab8ded6161e68616b43814549faa7d50
北䑓如法 <algebraicallyClosedField@gmail.com>
2023-08-10 00:24:22 +0900

    Add AstroCompress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
 export default defineConfig({
-    base: '/nyoho/kimura60'
+  base: '/nyoho/kimura60',
+  integrations: [compress()]
 });
